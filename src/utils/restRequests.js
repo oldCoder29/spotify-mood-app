e5c665696dea43207389408a361ef971534deb11
jaiibishnoi <jaipal@switchon.io>
2022-03-07 18:46:43 +0530

    added source code

--- src/utils/restRequests.js
+++ src/utils/restRequests.js
@@ -1,15 +1,5 @@
 var axios = require('axios')
 
-var config = {
-  method: 'get',
-  url: 'https://api.spotify.com/v1/search?q=delhi&type=playlist&limit=1',
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer BQCh0RHNFiIBstRAbDTFEAH6LobIG5POAI84xfoSB6VQz4VvtjaJkNeLr5p53FShSoFzzrx0C6IeOECZM2ZQ0AQmQ6_sV8OdHLdqi3LV7tQYfMDsrrYMkLNeuQ_cTH2OdN87G5Gj_gEJXinv2UYe6aVJVnLasRVaGg3RK8iweGNXXI70uWGu0TvorXs-DALtuWNnkufpz4mOPe5Sux-VRFpiM0lsBLk3xkoyydNqCJ73duUNeXKzKTMndggZ7WLPUicJgg1K0Y_lcFUp8o929lArC_bZCNat8saj7gkZxY36'
-  }
-}
-
 module.exports = async (config) => {
   try {
     const response = await axios(config)
